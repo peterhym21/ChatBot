36389bdf0c76191c018b1997d4787f273372ecdf
Peter Hymøller <pete102c@elevcampus.dk>
2022-10-18 14:57:14 +0200

    Add project files.

---
+++ MLModelIntent/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MLModelIntent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
