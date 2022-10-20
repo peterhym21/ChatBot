e84afe03bab8f83c059e2ad02b12615223e23c1f
Peter Hymøller <pete102c@elevcampus.dk>
2022-10-20 13:32:04 +0200

    some what working respondes

---
+++ ChatBotWeb/Data/ChatbotGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatBotWeb.Data
+{
+    public class ChatbotGroup
+    {
+        public string IntentGroup { get; set; } 
+
+        public List<ChatbotText> Text { get; set; }
+    }
+}
---
+++ ChatBotWeb/Data/ChatbotText.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatBotWeb.Data
+{
+    public class ChatbotText
+    {
+
+        public string Utterances { get; set; }
+        public string Intent { get; set; }
+        public string FocusArea { get; set; }
+
+
+    }
+}
